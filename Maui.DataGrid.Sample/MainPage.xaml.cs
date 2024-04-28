827693c7d2ffee9dee3c3bbff074197f3704a137
lendres <lanceendres@gmail.com>
2024-04-28 12:10:24 -0500

    Item selected test and release

--- Maui.DataGrid.Sample/MainPage.xaml.cs
+++ Maui.DataGrid.Sample/MainPage.xaml.cs
@@ -1,6 +1,9 @@
 namespace Maui.DataGrid.Sample;
 
+using CommunityToolkit.Maui.Views;
+using Maui.DataGrid.Sample.Models;
 using Maui.DataGrid.Sample.ViewModels;
+using System.Diagnostics;
 
 [XamlCompilation(XamlCompilationOptions.Compile)]
 public partial class MainPage
@@ -13,5 +16,19 @@ public partial class MainPage
         {
             Columns = _dataGrid1.Columns
         };
+
+        _dataGrid1.ItemSelected += DataGrid1_ItemSelected;
+    }
+
+    private async void OnSettingsClicked(object sender, EventArgs e)
+    {
+        var vm = (MainViewModel)BindingContext;
+        var settingsPopup = new SettingsPopup(vm);
+        _ = await Shell.Current.ShowPopupAsync(settingsPopup);
+    }
+
+    private void DataGrid1_ItemSelected(object? sender, SelectionChangedEventArgs e)
+    {
+        Debug.WriteLine($"Current selection: {((Team)e.CurrentSelection[0]).Name}");
     }
 }
